187bbaa22e333624bcb9618f7c2fe19892969d4b
zeus <zeus.ramirez347@gmail.com>
2021-05-19 00:45:35 -0400

    commit tree 63bdc0277cb6f6f4b3384044a9bab8c1815e9997

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+        return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(outerParam="*"){
+    return function (innerParam="special"){
+        return(`You are ${outerParam}${innerParam}${outerParam}!`)
+    }
+}
